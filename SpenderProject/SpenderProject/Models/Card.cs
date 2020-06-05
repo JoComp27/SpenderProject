8cb1c535d559c9d9077ac273802ddad7bced4c31
Jonathan <jonathan.cournoyer27@gmail.com>
2020-06-05 11:20:01 -0400

    Started Creating Models for game

---
+++ SpenderProject/SpenderProject/Models/Card.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpenderProject.Models
+{
+    class Card
+    {
+        public Colors Color { get; }
+
+        public int Points { get; }
+
+        public int WhiteCost { get; }
+
+        public int BlueCost { get; }
+
+        public int GreenCost { get; }
+
+        public int RedCost { get; }
+
+        public int BlackCost { get; }
+
+        public Card(Colors Color, int Points, int WhiteCost, int BlueCost, int GreenCost, int RedCost, int BlackCost)
+        {
+            this.Color = Color;
+            this.Points = Points;
+
+            this.WhiteCost = WhiteCost;
+            this.BlueCost = BlueCost;
+            this.GreenCost = GreenCost;
+            this.RedCost = RedCost;
+            this.BlackCost = BlackCost;
+        }
+
+
+    }
+}
---
+++ SpenderProject/SpenderProject/Models/Player.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpenderProject.Models
+{
+    class Player
+    {
+
+        public int Score { get; set; }
+
+        public int WhiteCoins { get; set; }
+        public int BlueCoins { get; set; }
+        public int GreenCoins { get; set; }
+        public int RedCoins { get; set; }
+        public int BlackCoins { get; set; }
+        public int WildCoins { get; set; }
+
+        public int WhiteCards { get; set; }
+        public int BlueCards { get; set; }
+        public int GreenCards { get; set; }
+        public int RedCards { get; set; }
+        public int BlackCards { get; set; }
+
+        public void buyCard(Card card)
+        {
+            if (IsCardBuyable(card))
+            {
+                Score += card.Points;
+            }
+            else
+            {
+                Console.WriteLine("Trying to buy a card you can't afford");
+            }
+        }
+
+        public Boolean IsCardBuyable(Card card)
+        {
+            return false;
+        }
+
+        public int getSumOfCoins()
+        {
+            return WhiteCoins + BlueCoins + GreenCoins + RedCoins + BlackCoins + WildCoins;
+        }
+
+    }
+}
